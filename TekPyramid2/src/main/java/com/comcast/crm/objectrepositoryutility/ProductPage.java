b941496666c5472298d4c8cab8e85baf3ca263e4
hp <hp@DESKTOP-UKFMVAS.mshome.net>
2025-07-04 04:45:45 -0700

    product pom class is added

---
+++ TekPyramid2/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -0,0 +1,12 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage {
+	
+	@FindBy(linkText = "link11")
+	private WebElement el2;
+
+
+}
