dbf2ad20e2c183dd805acb4511dc9e16ac60109c
hp <hp@DESKTOP-UKFMVAS.mshome.net>
2025-07-04 04:47:01 -0700

    Merge branch 'master' of https://github.com/rahulme0066/CrmComCastFramework.git

---
+++ TekPyramid2/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage {
+	
+	@FindBy(linkText = "link")
+	private WebElement el3;
+
+}
